0dfc8ad58a3ea9c75e9e108b91fccf14e048eefd
Darbaz Ali <darbaz.me@gmail.com>
2020-12-27 15:37:08 +0300

    responsive navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -6,9 +6,20 @@ import Home from './screens/Home'
 import About from './screens/About'
 
 const App = () => {
+  const [showHeader, setShowHeader] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      let scrollTop = window.scrollY
+      if (scrollTop > 100) {
+        setShowHeader(true)
+      }
+    })
+  }, [showHeader])
+
   return (
     <Router>
-      <Header />
+      {showHeader ? <Header /> : ''}
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
