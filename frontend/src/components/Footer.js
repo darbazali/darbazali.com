fe08032e2a41bde6ec91595772f2e76344bee6db
Darbaz Ali <darbaz.me@gmail.com>
2021-01-07 16:31:08 +0300

    new footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,45 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import FlexContainer from './FlexContainer'
-
-import { Github, Linkedin } from '@icons-pack/react-simple-icons'
 
 const footerStyle = {
-  width: '100%',
-  height: '160px',
-  background: '#ebebeb',
-  padding: '50px 0',
+  width: '410px',
+  height: '100px',
+  background: 'transparent',
+  padding: '20px 0',
+  borderTop: '1px solid #888888',
+  margin: '0 auto',
+  marginTop: '50px',
+  textAlign: 'center',
+  opacity: '0.8',
 }
 
-const Footer = () => (
-  <footer style={footerStyle}>
-    <FlexContainer
-      container
-      alignItems='center'
-      justifyContent='space-between'
-      alignContent='center'
-      flexWrap='nowrap'
-      maxWidth='1240px'
-      margin='0 auto'
-      padding='10px 0'
-      className='footer'
-    >
-      <div>
-        {/* <Link to='/#contact'>Contact</Link> */}
-        <Link to='/resume'>Resume</Link>
-      </div>
-      <p>© 2020 - Darbaz Ali</p>
-
-      <div className='socials'>
-        <a href='https://github.com/DarbazAli'>
-          <Github size={44} fill='#697285' />
-        </a>
-        <a href='https://www.linkedin.com/in/darbaz/'>
-          <Linkedin size={44} fill='#697285' />
-        </a>
-      </div>
-    </FlexContainer>
-  </footer>
-)
+const Footer = () => <footer style={footerStyle}>© 2020 - Darbaz Ali</footer>
 
 export default Footer
