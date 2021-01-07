af430f2fa3628a060fc117cb6fdeb34eb24b2a9e
Darbaz Ali <darbaz.me@gmail.com>
2021-01-07 15:42:24 +0300

    contact form mod

--- frontend/src/sections/Contact.js
+++ frontend/src/sections/Contact.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { Github, Linkedin } from '@icons-pack/react-simple-icons'
+
 import Button from '../components/Button'
+
 import '../styles/contact.css'
 
 import FlexContainer from '../components/FlexContainer'
@@ -35,68 +38,83 @@ const Contact = () => {
   }
   return (
     <section className='contact' id='contact'>
-      <FlexContainer
-        container
-        alignItems='center'
-        alignContent='center'
-        justifyContent='space-around'
-      >
+      <div className='container'>
         <ContactMessage />
-        <form onSubmit={handleSendEmail}>
-          <div className='field'>
-            <input
-              type='email'
-              name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              id='email'
-              placeholder='agent@station.com'
-            ></input>
-            <label htmlFor='email'>Email</label>
-          </div>
 
-          <div className='field'>
-            <input
-              type='text'
-              name='subject'
-              value={subject}
-              onChange={(e) => setSubject(e.target.value)}
-              id='subject'
-              placeholder='Aliens are comming'
-            ></input>
-            <label htmlFor='subject'>Subject</label>
-          </div>
+        <div className='col'>
+          <form onSubmit={handleSendEmail}>
+            <h5>Send a message</h5>
+            <div className='field'>
+              <input
+                type='email'
+                name='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                id='email'
+                placeholder='agent@station.com'
+              ></input>
+              <label htmlFor='email'>Email</label>
+            </div>
 
-          <div className='field'>
-            <textarea
-              name='message'
-              id='message'
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              placeholder='Prepare Raytheon MIM-23 HAWK for counter lunch!'
-            ></textarea>
-            <label htmlFor='message'>Message</label>
-          </div>
+            <div className='field'>
+              <input
+                type='text'
+                name='subject'
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                id='subject'
+                placeholder='Aliens are comming'
+              ></input>
+              <label htmlFor='subject'>Subject</label>
+            </div>
 
-          <Button className='button' type='submit' onClick={handleSendEmail}>
-            {loading ? 'Sending..' : 'Send Message'}
-          </Button>
-        </form>
-      </FlexContainer>
+            <div className='field'>
+              <textarea
+                name='message'
+                id='message'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder='Prepare Tomahawk for counter lunch!'
+              ></textarea>
+              <label htmlFor='message'>Message</label>
+            </div>
+
+            <Button className='button' type='submit' onClick={handleSendEmail}>
+              {loading ? 'Sending..' : 'Send Message'}
+            </Button>
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
 
 const ContactMessage = () => {
   return (
-    <div style={{ width: '300px' }}>
-      <h3>Contact</h3>
-      <p>
+    <div className='contact-message col'>
+      <h3>Get in touch</h3>
+      <p style={{ marginBottom: '70px', maxWidth: '300px' }}>
         Whether you have a product/service problem, or just wanna say hi , feel
         free to shoot me an email, and I will be in touch asap.
       </p>
+
+      <hr style={{ marginBottom: '20px', maxWidth: '300px' }} />
+
+      <h5>Find me on</h5>
+      <Socials />
     </div>
   )
 }
+
+const Socials = () => (
+  <div className='socials'>
+    <a href='https://github.com/DarbazAli' style={{ marginRight: '2em' }}>
+      <Github size={44} fill='#697285' />
+    </a>
+    <a href='https://www.linkedin.com/in/darbaz/'>
+      <Linkedin size={44} fill='#697285' />
+    </a>
+  </div>
+)
 
 export default Contact
