eb9365e7ed10e4835c5e45909c72e4098948badb
Darbaz Ali <darbaz.me@gmail.com>
2021-01-22 16:56:39 +0300

    remove unused packages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import 'normalize.css'
-// import './index.css'
+
 import './styles/style.css'
 import App from './App'
-
-import smoothscroll from 'smoothscroll-polyfill'
-
-// kick off the polyfill!
-smoothscroll.polyfill()
-window.__forceSmoothScrollPolyfill__ = true
 
 ReactDOM.render(
   <React.StrictMode>
